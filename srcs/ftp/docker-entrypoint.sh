e323a9b89855273f3cd1b47456c51da478abf9e8
Loïc Humbert <loic@humbert-web.dev>
2023-08-24 19:29:31 +0200

    Added FTP and security fix for passwords in images

---
+++ srcs/ftp/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+if ! id -u $FTP_USER >/dev/null 2>&1; then
+    adduser --gecos "" --disabled-password ${FTP_USER}
+    echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+    echo ${FTP_USER} | tee -a /etc/vsftpd.userlist
+fi
+
+exec "$@"
