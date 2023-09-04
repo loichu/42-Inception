cd5e632645f03f996563bfc2b0d7cd154bdbca5c
Loïc Humbert <loic@humbert-web.dev>
2023-09-05 00:32:04 +0200

    mkdocs OK

--- srcs/ftp/docker-entrypoint.sh
+++ srcs/ftp/docker-entrypoint.sh
@@ -5,6 +5,7 @@ if ! id -u $FTP_USER >/dev/null 2>&1; then
     adduser --gecos "" --disabled-password ${FTP_USER}
     echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
     echo ${FTP_USER} | tee -a /etc/vsftpd.userlist
+    usermod -aG www-data ftpuser
 fi
 
 exec "$@"
